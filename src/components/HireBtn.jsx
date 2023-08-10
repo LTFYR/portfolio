9275c63f6045d0c92a60d47faf0bd7c09c067ffb
LTFYR <lutfiyariea@code.edu.az>
2023-08-10 11:45:59 +0400

    changes

--- src/components/HireBtn.jsx
+++ src/components/HireBtn.jsx
@@ -9,7 +9,7 @@ const HireBtn = () => {
         <CircularText className="fill-white animate-spin-custom dark:fill-white" />
         <Link
           href="mailto:lutfiyar888lutfiyar@gmail.com"
-          className="w-20 h-20 rounded-full flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[rgba(253,29,29,1)] text-white shadow-md border border-solid border-none hover:bg-black hover:text-white dark:bg-white dark:text-dark dark:font-semibold hover:font-semibold md:w-12 md:h-12 md:text-[10px]"
+          className="w-20 h-20 rounded-full flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-[rgba(253,29,29,1)] text-white shadow-md border border-none hover:bg-black hover:text-white dark:bg-white dark:text-dark dark:font-semibold hover:font-semibold md:w-12 md:h-12 md:text-[10px]"
         >
           Hire me
         </Link>
